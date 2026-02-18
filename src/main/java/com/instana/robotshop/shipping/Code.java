bc05e0bf3085ade58612927ca6dd5da492cc55b3
Guru Theja <gurutejashetty@gmail.com>
2026-02-18 19:54:20 +0530

    Auto Push

--- src/main/java/com/instana/robotshop/shipping/Code.java
+++ src/main/java/com/instana/robotshop/shipping/Code.java
@@ -3,7 +3,7 @@ package com.instana.robotshop.shipping;
 import javax.persistence.Table;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
+import javax.persistence.GenerationType
 import javax.persistence.Id;
 
 /*
